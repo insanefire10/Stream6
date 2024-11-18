98314dcfe01c5a2376bcfb3cac3b9712f06917d6
insanefire10 <36336394+insanefire10@users.noreply.github.com>
2024-11-18 12:31:54 -0500

    Autodeletion after 1hr now works

--- clearsongs.py
+++ clearsongs.py
@@ -1,9 +1,17 @@
 import asyncio
 import time
+import glob
+import os
 
 
 def clear_songs():
     while True:
-        time.sleep(300)
+        time.sleep(3600)
         print("Deleting Songs")
-        folder_path = './audiofiles'
+        folder_path = './audiofiles/*'
+        files = glob.glob(folder_path)
+        for mp3 in files:
+            try:
+                os.remove(mp3)
+            except:
+                print(mp3 + " is in use")
